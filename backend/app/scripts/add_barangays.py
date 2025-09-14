5cef18e3e987a721d4436e9ec3234d8342d2c68a
eigenlambda123 <rmvilla987@gmail.com>
2025-09-14 20:28:33 +0800

    update add_barangays script

--- backend/app/scripts/add_barangays.py
+++ backend/app/scripts/add_barangays.py
@@ -1,5 +1,6 @@
 from app.models import Barangay
 from app.db import get_session
+from sqlalchemy import text
 
 barangays_data = [
     {"id": 1, "name": "Barangay Dalahican, Lucena", "lat": 13.9317, "lon": 121.6233},
@@ -9,6 +10,7 @@ barangays_data = [
 def main():
     # get_session is a generator, so use next() to get the session
     session = next(get_session())
+    session.exec(text("DELETE FROM barangay"))
     for b in barangays_data:
         barangay = Barangay(id=b["id"], name=b["name"], lat=b["lat"], lon=b["lon"])
         session.add(barangay)
