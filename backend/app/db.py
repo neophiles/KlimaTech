d99456523765b232ea92216841b48e0bc830024d
eigenlambda123 <rmvilla987@gmail.com>
2025-09-14 19:58:54 +0800

    switched to postgre database

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,16 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./heat_project.db"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
@@ -9,3 +19,4 @@ def init_db():
 def get_session():
     with Session(engine) as session:
         yield session
+    
