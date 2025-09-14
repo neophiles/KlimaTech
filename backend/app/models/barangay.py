8e794cd2f9b4d8ba57d9c47c70be762d134d5a24
eigenlambda123 <rmvilla987@gmail.com>
2025-09-14 18:33:05 +0800

    SQLModel requirements.txt

---
+++ backend/app/models/barangay.py
@@ -0,0 +1,10 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+
+class Barangay(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    lat: float
+    lon: float
+
+    heat_logs: List["HeatLog"] = Relationship(back_populates="barangay")
