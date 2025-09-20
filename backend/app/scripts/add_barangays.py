c88f83c11d86f455522f26afdb254426a3e5cc9e
eigenlambda123 <rmvilla987@gmail.com>
2025-09-20 16:58:35 +0800

    refactor api structure for add_barangays.py script

--- backend/app/scripts/add_barangays.py
+++ backend/app/scripts/add_barangays.py
@@ -3,16 +3,36 @@ from app.db import get_session
 from sqlalchemy import text
 
 barangays_data = [
-    {"id": 1, "name": "Barangay Ibabang Dupay, Lucena, Quezon", "lat": 13.9405, "lon": 121.617},
-    {"id": 2, "name": "Bgry. Ikirin, Pagbilao, Quezon", "lat": 13.9968, "lon": 121.7309},
+    {
+        "id": 1,
+        "barangay": "Ibabang Dupay",
+        "locality": "Lucena",
+        "province": "Quezon",
+        "lat": 13.9405,
+        "lon": 121.617
+    },
+    {
+        "id": 2,
+        "barangay": "Ikirin",
+        "locality": "Pagbilao",
+        "province": "Quezon",
+        "lat": 13.9968,
+        "lon": 121.7309
+    },
 ]
 
 def main():
-    # get_session is a generator, so use next() to get the session
     session = next(get_session())
     session.exec(text("DELETE FROM barangay"))
     for b in barangays_data:
-        barangay = Barangay(id=b["id"], name=b["name"], lat=b["lat"], lon=b["lon"])
+        barangay = Barangay(
+            id=b["id"],
+            barangay=b["barangay"],
+            locality=b["locality"],
+            province=b["province"],
+            lat=b["lat"],
+            lon=b["lon"]
+        )
         session.add(barangay)
     session.commit()
     print("Barangays added!")
