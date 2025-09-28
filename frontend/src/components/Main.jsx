c15837eb3e6a3f46b4d5cde865f423702508828b
neophiles <mnv.tagle@gmail.com>
2025-09-28 19:06:35 +0800

    relocate routes in Main

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,9 +1,16 @@
 import { Dashboard } from '../pages/Dashboard'
+import { HeatMap } from "../pages/HeatMap";
+import { Routes, Route } from "react-router-dom";
 
 export function Main() {
     return (
         <main>
-            <Dashboard />
+            <Routes>
+                {/* Default page */}
+                <Route path="/" element={<Dashboard />} />
+                {/* Heatmap page */}
+                <Route path="/heatmap" element={<HeatMap />} />
+            </Routes>
         </main>
     );
 }
