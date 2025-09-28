78d5a63579e278a308f5e074b4e8e8c3be889c9f
neophiles <mnv.tagle@gmail.com>
2025-09-28 19:04:40 +0800

    remove routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,26 +3,16 @@ import 'leaflet/dist/leaflet.css'
 import { Header } from './components/Header'
 import { Main } from './components/Main'
 import { Footer } from './components/Footer'
-import HeatMap from "./pages/HeatMap";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Header />
-      
-
-      <Routes>
-        {/* Default page */}
-        <Route path="/" element={<Main />} />
-        {/* Heatmap page */}
-        <Route path="/heatmap" element={<HeatMap />} />
-      </Routes>
-
+      <Main />
       <Footer />
     </Router>
-  )
+  );
 }
 
 export default App
