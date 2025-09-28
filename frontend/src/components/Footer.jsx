c63038bd0507403d6abb060c34eb62a8b8d2ce7e
neophiles <mnv.tagle@gmail.com>
2025-09-28 18:00:34 +0800

    add NavigationButton instances

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,22 @@
+import { NavigationButton } from './NavigationButton';
+
 export function Footer() {
     return (
         <footer>
+            <NavigationButton
+                route={'/'} 
+                label={'Home'}
+            />
+            <NavigationButton
+                route={'/heatmap'}
+                label={'Map'}
+            />
+            <NavigationButton
+                label={'Analysis'}
+            />
+            <NavigationButton
+                label={'Settings'}
+            />
         </footer>
     );
 }
