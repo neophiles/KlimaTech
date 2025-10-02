353154455a225b9db9c14aaf8eb29387df153d01
neophiles <mnv.tagle@gmail.com>
2025-10-02 19:07:22 +0800

    modify formatHourLabel

--- frontend/src/scripts/utils.js
+++ frontend/src/scripts/utils.js
@@ -1,9 +1,9 @@
 export function getIndexByHeat(heatIndex) {
-    if (heatIndex < 27) return 0; // very cool
-    if (heatIndex < 32) return 1; // cool
-    if (heatIndex < 41) return 2; // moderate
-    if (heatIndex < 54) return 3; // hot
-    return 4; // extreme
+    if (heatIndex < 27) return 0; // safe
+    if (heatIndex < 32) return 1; // caution
+    if (heatIndex < 41) return 2; // extreme caution
+    if (heatIndex < 54) return 3; // danger
+    return 4; // extreme danger
 }
 
 export function getColorByIndex(index) {
@@ -21,11 +21,17 @@ export function formatFullTime(timestamp) {
         : "--:--";
 }
 
-export function formatHourLabel(timestamp) {
-    return timestamp
-        ? new Date(timestamp).toLocaleTimeString([], {
-            hour: "numeric",
-            hour12: true
-        })
-        : "--";
+export function formatHourLabel(timestamp, start, end) {
+    if (!timestamp) return "--";
+
+    const date = new Date(timestamp);
+    const hour = date.getHours(); // 0–23
+
+    // Only allow 5 AM (5) through 7 PM (19)
+    if (hour < start || hour > end) return;
+
+    return date.toLocaleTimeString([], {
+        hour: "numeric",
+        hour12: true
+    });
 }
