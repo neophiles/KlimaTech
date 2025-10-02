f95abb553617eab5ac4ab8b1aecc1284f069736f
neophiles <mnv.tagle@gmail.com>
2025-10-02 19:36:51 +0800

    add color parameter

--- frontend/src/components/TimeSlot.jsx
+++ frontend/src/components/TimeSlot.jsx
@@ -1,8 +1,11 @@
-export function TimeSlot({ time, heatIndex }) {
+export function TimeSlot({ time, heatIndex, color }) {
     return (
         <div className="slot-column">
             <span>{time}</span>
-            <div className={`slot-capsule ${heatIndex < 33 ? "safe" : "hot"}`}>
+            <div
+                className="slot-capsule"
+                style={{backgroundColor: color}}
+            >
                 <span>{heatIndex}°C</span>
                 <span>🌥️</span>
             </div>
