763d725e56e2ba27de3df1b44a10846faf7b22df
neophiles <mnv.tagle@gmail.com>
2025-10-03 22:33:57 +0800

    add dynamic slot capsule y offsets

--- frontend/src/components/TimeSlot.jsx
+++ frontend/src/components/TimeSlot.jsx
@@ -1,10 +1,17 @@
-export function TimeSlot({ time, heatIndex, color }) {
+import { getColorByIndex } from "../scripts/utils";
+
+export function TimeSlot({ time, heatIndex, index }) {
+    const yOffsets = [20, 10, 0, -10, 20];
+
     return (
         <div className="slot-column">
             <span>{time}</span>
             <div
                 className="slot-capsule"
-                style={{backgroundColor: color}}
+                style={{
+                    backgroundColor: getColorByIndex(index),
+                    transform: `translateY(${yOffsets[index]}px)`
+                }}
             >
                 <span>{heatIndex}°C</span>
                 <span>🌥️</span>
