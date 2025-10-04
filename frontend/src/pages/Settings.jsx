75c39cbfaf68ecbd06c8cd634a7057cba488edf4
neophiles <mnv.tagle@gmail.com>
2025-10-04 13:41:39 +0800

    add Settings page component

---
+++ frontend/src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import GuideWidget from "../components/GuideWidget";
+
+function Settings() {
+    return (
+        <div className="settings">
+            <h1>Settings</h1>
+            <GuideWidget />
+        </div>
+    );
+}
+
+export default Settings;
