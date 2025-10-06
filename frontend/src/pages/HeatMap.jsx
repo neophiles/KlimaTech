92be6f05f96269aa6eb0837abab02a975ca04538
neophiles <mnv.tagle@gmail.com>
2025-10-06 13:28:20 +0800

    change add marker button icon

--- frontend/src/pages/HeatMap.jsx
+++ frontend/src/pages/HeatMap.jsx
@@ -163,7 +163,13 @@ function HeatMap() {
       )}
 
       {/* Button to enable add mode */}
-      <Button children={addMode ? "Click on the map to add a cool spot..." : "Add Cool Spot"} onClick={setAddMode} />
+      <Button 
+        children={
+          <svg className="nav-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+          </svg>
+        } 
+        onClick={setAddMode} />
     </div>
   );
 }
