bfc6a769568ef340dd4ccb3881878430a106f846
eigenlambda123 <rmvilla987@gmail.com>
2025-10-07 07:41:01 +0800

    update AddSpotOnClick to actually use the data in the form using formData

--- frontend/src/components/coolspots/AddSpotOnClick.jsx
+++ frontend/src/components/coolspots/AddSpotOnClick.jsx
@@ -1,17 +1,24 @@
 import { useMapEvents } from "react-leaflet";
 
-function AddSpotOnClick({ addMode, newSpotType, onAddSpot }) {
+
+function AddSpotOnClick({ addMode, pendingSpot, onAddSpot }) {
   useMapEvents({
     click(e) {
-      if (addMode) {
-        const newSpot = {
-          barangay_id: 1,
-          name: "Cool Spot",
-          type: newSpotType,
-          lat: e.latlng.lat,
-          lon: e.latlng.lng
-        };
-        onAddSpot(newSpot);
+      if (addMode && pendingSpot) {
+        const formData = new FormData();
+        formData.append("barangay_id", pendingSpot.barangay_id); 
+        formData.append("name", pendingSpot.name);
+        formData.append("type", pendingSpot.type);
+        formData.append("lat", e.latlng.lat);
+        formData.append("lon", e.latlng.lng);
+        if (pendingSpot.photo) formData.append("file", pendingSpot.photo);
+
+        // Debug: Log FormData entries
+        for (let pair of formData.entries()) {
+          console.log(pair[0]+ ': ' + pair[1]);
+        }
+
+        onAddSpot(formData); // Pass FormData to handler
       }
     }
   });
