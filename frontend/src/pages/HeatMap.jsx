60d3d000c5e9bc610ac4e9f6613da3019941707f
neophiles <mnv.tagle@gmail.com>
2025-10-12 18:16:10 +0800

    change add coolspot button text to plus icon

--- frontend/src/pages/HeatMap.jsx
+++ frontend/src/pages/HeatMap.jsx
@@ -252,9 +252,14 @@ function HeatMap() {
       )}
 
       {/* Button to enable add mode */}
-      <Button onClick={() => setShowAddModal(true)}>
-        {addMode ? "Click on the map to add a cool spot..." : "Add Cool Spot"}
-      </Button>
+      <Button 
+        onClick={() => setShowAddModal(true)}
+        children={
+          <svg className="nav-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+          </svg>
+        }
+      />
 
       {/* Modal for adding new cool spot */}
       {showAddModal && (
