01b76a7ce0cac403061c97d98c03027305752937
neophiles <mnv.tagle@gmail.com>
2025-10-16 12:44:08 +0800

    remove unneeded widgets

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import AdvisoryWidget from "../components/widgets/AdvisoryWidget";
 import BriefingsWidget from "../components/widgets/BriefingsWidget/BriefingsWidget";
 import HeatClockWidget from "../components/widgets/HeatClockWidget/HeatClockWidget";
 import ErrorWidget from "../components/widgets/ErrorWidget";
+import Clock from "../components/Clock/Clock";
 
 function Dashboard({ userData }) {
   const [weatherData, setWeatherData] = useState(null);
@@ -74,16 +75,14 @@ function Dashboard({ userData }) {
 
   return (
     <div className="dashboard">
-      <HeatGauge heatIndex={heat_index} timestamp={updated_at} />
+      <Clock riskLevel={risk_level} />
       <LocationWidget barangay={barangay} locality={locality} province={province} />
-      <AdvisoryWidget heatIndex={heat_index} riskLevel={risk_level} advice={advice} />
       <BriefingsWidget
         temperature={temperature}
         humidity={humidity}
         wind_speed={wind_speed}
         uv_index={uv_index}
       />
-      <HeatClockWidget />
     </div>
   );
 }
