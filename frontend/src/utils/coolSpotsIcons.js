d6955550cfb4efd00f390cb1032f8b07b4da2d96
neophiles <mnv.tagle@gmail.com>
2025-10-16 15:20:36 +0800

    add custom preskospot marker icon

--- frontend/src/utils/coolSpotsIcons.js
+++ frontend/src/utils/coolSpotsIcons.js
@@ -1,5 +1,6 @@
 import L from "leaflet";
-import youIcon from "../../public/icons_png/you-icon.png";
+import youIcon from "/icons_png/you-icon.png";
+import preskoSpotIcon from "/icons_png/preskospot-marker.png";
 
 export const userIcon = new L.Icon({
   iconUrl: youIcon,
@@ -9,4 +10,13 @@ export const userIcon = new L.Icon({
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
   shadowSize: [40, 40],
+});
+
+export const preskoSpotMarker = new L.Icon({
+  iconUrl: preskoSpotIcon,
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize:     [25, 41],
+  iconAnchor:   [12, 41],
+  popupAnchor:  [1, -34],
+  shadowSize:   [41, 41]
 });
