09ad31d803897813e6b5db068c13e0e340b2046b
neophiles <mnv.tagle@gmail.com>
2025-10-18 09:58:22 +0800

    improve header, add logo redirect functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
 function Header() {
+    const navigate = useNavigate();
+
+    const handleGoToHome = () => {
+        navigate("/");
+    };
+
     return (
         <header>
-            <img className="logo" src="/logo/name_logo.png" alt="PRESKO LOGO" />
+            <div className="header-container">
+                <Button
+                    otherClass={"logo-btn"}
+                    onClick={handleGoToHome}
+                    children={
+                        <>
+                            <img className="logo" src="/logo/presko-logo.png" alt="PRESKO LOGO" />
+                            <span className="title">PRESKO</span>
+                        </>
+                    }
+                />   
+            </div>
         </header>
     );
 }
