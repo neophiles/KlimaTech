a535635807d84e12db889c1f2ed225227fde94b2
neophiles <mnv.tagle@gmail.com>
2025-10-21 16:20:51 +0800

    adjust preskoSpotMarker popupAnchor

--- frontend/src/utils/coolSpotsIcons.js
+++ frontend/src/utils/coolSpotsIcons.js
@@ -6,14 +6,14 @@ export const userIcon = new L.Icon({
   iconUrl: youIcon,
   iconSize: [15, 15],
   iconAnchor: [7.5, 15],
-  popupAnchor: [1, -35],
+  popupAnchor: [1, -10],
 });
 
 export const preskoSpotMarker = new L.Icon({
   iconUrl: preskoSpotIcon,
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-  iconSize:     [25, 41],
-  iconAnchor:   [12, 41],
-  popupAnchor:  [1, -34],
-  shadowSize:   [41, 41]
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -25],
+  shadowSize: [41, 41]
 });
