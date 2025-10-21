856633a7f8ee1aca330f568bd46e7b88b2963bc1
neophiles <mnv.tagle@gmail.com>
2025-10-21 21:57:54 +0800

    change coolspot-details icon

--- frontend/src/components/map/markers/PreskoSpotPopupContent.jsx
+++ frontend/src/components/map/markers/PreskoSpotPopupContent.jsx
@@ -146,7 +146,10 @@ setSelectedSpot(prev => {
             <span className="coolspot-desc">{spot.type}</span>
             <button className="coolspot-details-btn" onClick={() => onViewDetails(spot.id)}>
             <svg className="nav-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15M3 12h18" />
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M4 6l16 0" />
+              <path d="M4 12l16 0" />
+              <path d="M4 18l16 0" />
             </svg>
             </button>
         </div>
