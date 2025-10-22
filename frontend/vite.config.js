d9a06ebd8ac55e3cbc3b79ab8193c28183daa9b9
eigenlambda123 <rmvilla987@gmail.com>
2025-10-22 16:50:40 +0800

    add local backend url in proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // Replace this with your actual Render backend URL
 const BACKEND_URL = 'https://presko.onrender.com';
+const LOCALBACKEND_URL = 'http://localhost:8000';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      // All requests starting with /api will be forwarded to the backend
       '/api': {
         target: BACKEND_URL,
         changeOrigin: true,       // Makes the request appear as coming from the backend host
