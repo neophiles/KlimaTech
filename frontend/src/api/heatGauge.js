8c9f3206bd081e054ac5b083322dff11e5f3af6f
eigenlambda123 <rmvilla987@gmail.com>
2025-10-23 19:59:59 +0800

    added console.log

--- frontend/src/api/heatGauge.js
+++ frontend/src/api/heatGauge.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log(import.meta.env.VITE_API_BASE_URL);
 
 export async function fetchWeatherData(barangayId) {
   const res = await fetch(`${API_BASE_URL}/barangays/${barangayId}`);
