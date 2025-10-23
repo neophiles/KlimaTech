52791f1ff5ed998c8156122371114d58e43400ce
eigenlambda123 <rmvilla987@gmail.com>
2025-10-23 19:33:48 +0800

    fix some endpoints

--- frontend/src/api/heatGauge.js
+++ frontend/src/api/heatGauge.js
@@ -1,11 +1,13 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export async function fetchWeatherData(barangayId) {
-  const res = await fetch(`http://127.0.0.1:8000/barangays/${barangayId}`);
+  const res = await fetch(`${API_BASE_URL}/barangays/${barangayId}`);
   if (!res.ok) throw new Error('Failed to fetch weather data');
   return await res.json();
 }
 
 export async function fetchForecastData(barangayId) {
-  const res = await fetch(`http://127.0.0.1:8000/barangays/${barangayId}/forecast`);
+  const res = await fetch(`${API_BASE_URL}/barangays/${barangayId}/forecast`);
   if (!res.ok) throw new Error('Failed to fetch forecast data');
   return await res.json();
 }
