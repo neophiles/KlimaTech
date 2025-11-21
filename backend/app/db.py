6367ced5ae0e82a2611a2369c4b2d46e64cb23bb
eigenlambda123 <rmvilla987@gmail.com>
2025-11-21 22:52:53 +0800

    chore: static

--- backend/app/db.py
+++ backend/app/db.py
@@ -5,12 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # "dev" for development, "prod" for production
-ENV = os.getenv("ENVIRONMENT", "prod")
+ENV = os.getenv("ENV", "prod") 
 
 if ENV == "dev":
     # SQLite for development
     DATABASE_URL = "sqlite:///./heat_project.db"
+    engine = create_engine(DATABASE_URL, echo=True)
 else:
+    # # PostgreSQL for production
+    # DB_USER = os.getenv("DB_USER")
+    # DB_PASSWORD = os.getenv("DB_PASSWORD")
+    # DB_HOST = os.getenv("DB_HOST")
+    # DB_PORT = os.getenv("DB_PORT")
+    # DB_NAME = os.getenv("DB_NAME")
+    # DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
     # PostgreSQL for production - use the complete DATABASE_URL from Render
     DATABASE_URL = os.getenv("DATABASE_URL")
     
@@ -20,10 +30,9 @@ else:
     # Fix malformed URLs
     if ":None" in DATABASE_URL:
         DATABASE_URL = DATABASE_URL.replace(":None/", ":5432/")
+        
+    engine = create_engine(DATABASE_URL, echo=True)
 
-print(f"Using DATABASE_URL: {DATABASE_URL[:60]}...")
-
-engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
@@ -31,3 +40,4 @@ def init_db():
 def get_session():
     with Session(engine) as session:
         yield session
+
